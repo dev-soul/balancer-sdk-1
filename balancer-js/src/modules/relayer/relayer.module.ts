12846cec2880e2bb318d4fc8d3cbed3e900dff08
Daniel <danielBeets@protonmail.com>
2022-02-23 11:30:20 +0100

    augment the return amount with the unwrapped amount

--- balancer-js/src/modules/relayer/relayer.module.ts
+++ balancer-js/src/modules/relayer/relayer.module.ts
@@ -338,6 +338,27 @@ export class Relayer {
 
             additionalCalls = unwrapCalls;
             unwrapOutputReferences = outputReferences;
+
+            //update the return amounts to represent the unwrappedAmount
+            queryResult.returnAmounts = queryResult.returnAmounts.map(
+                (returnAmount, i) => {
+                    const asset = queryResult.assets[i];
+
+                    if (this.linearPoolWrappedTokenMap[asset]) {
+                        const linearPool =
+                            this.linearPoolWrappedTokenMap[asset];
+                        const priceRate =
+                            linearPool.tokens[linearPool.wrappedIndex || 0]
+                                .priceRate;
+
+                        return `${
+                            parseFloat(returnAmount) * parseFloat(priceRate)
+                        }`;
+                    }
+
+                    return returnAmount;
+                }
+            );
         }
 
         const encodedBatchSwap = Relayer.encodeBatchSwap({
@@ -357,7 +378,6 @@ export class Relayer {
             function: 'multicall',
             params: calls,
             outputs: {
-                //TODO: return amounts should get adjusted by wrapped token rates
                 amountsOut: queryResult.returnAmounts,
             },
         };
